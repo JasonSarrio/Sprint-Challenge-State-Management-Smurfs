4a7711691642371d725544dbc7c5e4013f27d32b
JasonSarrio <jsarrio16@gmail.com>
2020-03-06 18:54:23 -0600

    Working on project still adding to Smurflist

--- smurfs/src/components/SmurfsList.js
+++ smurfs/src/components/SmurfsList.js
@@ -20,7 +20,7 @@ const SmurfsList = props => {
 
     }
 
-    const handleHeight = e =>{
+    const handleHeight = e => {
         e.preventDefault();
         setHeight(e.target.value)
 
@@ -37,3 +37,47 @@ const SmurfsList = props => {
         props.getSmurf()
         props.addSmurf(name, age, height)
     }
+
+
+
+    return (
+        <div>
+
+            <form onSubmit={smurfGet}>
+                <label htmlFor='name'>Smurf Name:<br /><input onChange={handleName} id='name' name='name'></input></label><br />
+                <label htmlFor='age'>Smurf Age:<br /><input onChange={handleAge} id='age' name='age'></input></label><br />
+                <label htmlFor='height'>Smurf Height:<br /><input onChange={handleHeight} id='height' name='height'></input></label><br />
+                <button>Add Smurf</button>
+            </form>
+
+
+
+            <button onClick={smurfGet}>View Smurfs</button>
+
+            <div className='smurf-container'>
+                {props.smurfs.length ? props.smurfs.map(item => {
+                    return (
+                        <div className='smurf-bois'>
+                            <h3>{item.name}</h3>
+                            <p>Age: {item.age}</p>
+                            <p>Height: {item.height}</p>
+                            <button onClick={() => handleRemove(item.id)}>Remove Smurf</button>
+                        </div>
+                    )
+                }) : (<div>Let's add some Smurfs!</div>)}
+
+            </div>
+
+        </div>
+    )
+}
+
+const mapStateToProps = state => {
+    return {
+        smurfs: state.smurfs,
+        isFetching: state.isFetching,
+        error: state.error
+    }
+}
+
+export default connect(mapStateToProps, { getSmurf, addSmurf, removeSmurf })(SmurfsList)
